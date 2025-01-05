18b29f07de93c4fd09bb096d00709bb607f8d05d
vitalysementsov <v.sementsov@ratkus.com.ua>
2025-01-05 09:18:27 +0200

    update

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Параметры
+REPO_URL="git@github.com:ratkus91/mfisher.git"
+APP_DIR="/var/www/html/mfisher"
+
+# Шаги
+echo "Обновляем репозиторий..."
+if [ ! -d "$APP_DIR" ]; then
+    git clone $REPO_URL $APP_DIR
+else
+    cd $APP_DIR
+    git reset --hard
+    git pull origin main
+fi
+
+echo "Устанавливаем зависимости..."
+cd $APP_DIR
+npm install
+
+echo "Собираем проект..."
+npm run build
+
+echo "Запускаем сервер..."
+pm2 start dist --name "mfisher" --no-daemon
